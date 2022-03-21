957fb75f927068406f747739198703bccd5db0cc
Andres Celis <andresc@150porciento.com>
2022-03-20 19:35:17 -0500

    chore: update repo configs

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsup"
 
-export default defineConfig({
-    entry: ["./src/index.ts"],
-    clean: true,
-    dts: true,
-});
+import alias from "esbuild-plugin-alias"
+import path from "path"
+
+export default defineConfig(options => {
+    return {
+        entry: ["./src/index.ts"],
+        clean: true,
+        dts: true,
+        minify: !options.watch,
+        esbuildPlugins: [
+            alias({
+                $: path.resolve(__dirname, "./src/"),
+            }),
+        ],
+    }
+})
